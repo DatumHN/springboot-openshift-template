33287a7d389226f5f8f3f8d4996fb4f7297e7eb3
DatumHN <121521556+DatumHN@users.noreply.github.com>
2024-09-03 15:56:03 -0600

    Update GreetingEndpoint.java

--- src/main/java/dev/snowdrop/example/service/GreetingEndpoint.java
+++ src/main/java/dev/snowdrop/example/service/GreetingEndpoint.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Example;
 import io.swagger.annotations.ExampleProperty;
 import org.springframework.stereotype.Component;
@@ -21,10 +22,7 @@ public class GreetingEndpoint {
     @GET
     @Produces("application/json")
     @ApiOperation(tags = "Greeting", value = "Endpoint relacionado con los saludos", notes = "Servicio para obtener un saludo basado en un nombre ingresado.")
-    @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "{\"content\":\"Hola, Mundo!\"}"),
-        @ApiResponse(code = 400, message = "Parámetros inválidos")
-    })
+    @ApiResponse(code = 200, message = "{\"content\":\"Hola, Mundo!\"}")
     
     public Greeting greeting(@QueryParam("name") @DefaultValue("Mundo") String name) {
         final String message = String.format(Greeting.FORMAT, name);
