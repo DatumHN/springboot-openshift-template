016cec367e47443cba1b1fba07d2e275f9aa7d3d
DatumHN <121521556+DatumHN@users.noreply.github.com>
2024-09-03 15:17:56 -0600

    Update JmsConfig.java

--- src/main/java/dev/snowdrop/example/JmsConfig.java
+++ src/main/java/dev/snowdrop/example/JmsConfig.java
@@ -1,3 +1,5 @@
+package dev.snowdrop.example;
+
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
