ba36f41dd172aa3bceb22cd248ab24df0947f46e
DatumHN <121521556+DatumHN@users.noreply.github.com>
2024-09-03 15:19:46 -0600

    Create SpringFoxConfig.java

---
+++ src/main/java/dev/snowdrop/example/SpringFoxConfig.java
@@ -0,0 +1,45 @@
+package dev.snowdrop.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * HISTORIAL DE CAMBIOS
+ * Fecha                    Autor                  Descripción
+ * 03/09/2024               Melvin Cruz  		       Clase que será la encargada de renderizar el detalle Swagger de los servicios implementados en el proyecto.
+ **/
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+	@Value("${paquete.servicios}")
+	String paqueteApis;
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.OAS_30)  
+        .select()                                       
+        .apis(RequestHandlerSelectors.basePackage(paqueteApis))
+        .paths(PathSelectors.any())                     
+        .build().apiInfo(apiInfo());                                        
+    }
+	
+	 public ApiInfo apiInfo() { 
+	        return new ApiInfoBuilder()
+	            .title("Catalogos API")
+	            .description(
+	                "Api para obtener los registros de los catalogos en la Base de Datos de Clasificadores.")
+	            .version("1.0.0")
+	            .build();                                        
+	    }
+}
